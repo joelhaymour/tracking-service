9a5ba88318f4af26efa486f80c170511d2171536
joelhaymour <joelhaymour00@gmail.com>
2025-01-31 20:09:54 -0800

    Update shopify.js

--- src/config/shopify.js
+++ src/config/shopify.js
@@ -1,1 +1,9 @@
+const Shopify = require('shopify-api-node');
 
+const shopify = new Shopify({
+    shopName: process.env.SHOPIFY_SHOP_NAME,
+    accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
+    apiVersion: '2024-01'
+});
+
+module.exports = shopify;
