c5ca5a40455007247ea71b831e5855ece0391b29
joelhaymour <joelhaymour00@gmail.com>
2025-01-31 22:19:14 -0800

    Update shopify.js

--- src/config/shopify.js
+++ src/config/shopify.js
@@ -1,9 +1,14 @@
 const Shopify = require('shopify-api-node');
 
 const shopify = new Shopify({
-    shopName: process.env.SHOPIFY_SHOP_NAME,
+    shopName: '4c4148-3',
     accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
     apiVersion: '2024-01'
 });
 
-module.exports = shopify;
+// Add error handling for initialization
+shopify.on('error', (err) => {
+    console.error('Shopify API Error:', err);
+});
+
+module.exports = shopify; 
